05f902d29eb316e117a74d99e2faec52b74ff1a4
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-08-29 12:01:04 +0200

    base

---
+++ EvenNumbers.java
@@ -0,0 +1,13 @@
+package com.test.homework.for_loop;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+    /*
+    using for loop
+    print even number from 1 to 100 include
+    from new line
+     */
+    for (int i = 0; i < 100; i++) {
+        System.out.println(i);
+    }
+}}
