29e77f1f7b41a5e3f74bdd0e1fd971b4b10890f9
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-08-29 15:00:54 +0200

    base

---
+++ SSquare.java
@@ -0,0 +1,15 @@
+package com.test.homework.while_loop;
+
+public class SSquare {
+    /*
+    print to console a square 10*10 with 'S'
+    using loop while
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("SSSSSSSSSS");
+            i++;
+        }
+    }
+}
