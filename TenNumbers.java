78d8eec028c27dc2c9361d3d5f87eb54ce76c581
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-08-29 14:48:45 +0200

    base

---
+++ TenNumbers.java
@@ -0,0 +1,15 @@
+package com.test.homework.while_loop;
+
+public class TenNumbers {
+    public static void main(String[] args) {
+
+    /*
+    Print numbers from 1 to 10
+    using loop while
+     */
+    int i = 1;
+        while (i <= 10) {
+        System.out.println(i);
+        i++;
+}
+    }}
