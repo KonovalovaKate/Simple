97c1f20c1a336dd5a329d67471bf159140b8acfe
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-09-10 13:27:34 +0200

    base

--- com/test/homework/oop/inheritance/pet.java
+++ com/test/homework/oop/inheritance/pet.java
@@ -1,16 +1,29 @@
 package com.test.homework.oop.inheritance;
-
 public class pet {
-
+    protected String animal = "Animal";
+    public void honk() {
+        System.out.println("Animals:");
     }
 
+    public static void main(String[] args) {
+        cat myFastAnimal = new cat();
+        dog myFastAnimal2 = new dog();
+        myFastAnimal.honk();
+        System.out.println(myFastAnimal.animal + " " + myFastAnimal.petName);
+        System.out.println(myFastAnimal2.animal + " " + myFastAnimal2.petName2);
+
+    }
+}
+
 class cat extends pet {
+    String petName = "Cat";
+
 
     }
 class dog extends pet {
+    String petName2 = "Dog";
+
 
 }
-    public static void main(String[] args) {
 
-    }
-}
+
