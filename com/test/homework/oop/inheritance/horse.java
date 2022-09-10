6493fc06d846e0722aae167f1570ee7d4c619e93
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-09-10 13:15:14 +0200

    base

--- com/test/homework/oop/inheritance/horse.java
+++ com/test/homework/oop/inheritance/horse.java
@@ -10,9 +10,9 @@ class horse {
 class pegas extends horse {
     private String horseName = "Luna";
     public static void main(String[] args) {
-        pegas myFastCar = new pegas();
-        myFastCar.honk();
-        System.out.println(myFastCar.animal + " " + myFastCar.horseName);
+        pegas myFastAnimal = new pegas();
+        myFastAnimal.honk();
+        System.out.println(myFastAnimal.animal + " " + myFastAnimal.horseName);
     }
 }
 
---
+++ com/test/homework/oop/inheritance/pet.java
@@ -0,0 +1,16 @@
+package com.test.homework.oop.inheritance;
+
+public class pet {
+
+    }
+
+class cat extends pet {
+
+    }
+class dog extends pet {
+
+}
+    public static void main(String[] args) {
+
+    }
+}
