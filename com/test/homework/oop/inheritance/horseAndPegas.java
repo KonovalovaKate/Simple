d6d83dc9fc26085231f805ed2be728f45ec0bb7a
kate.konovalova <kate.konovalova.qa@gmail.com>
2022-09-10 13:00:36 +0200

    base

--- com/test/homework/oop/inheritance/horseAndPegas.java
+++ com/test/homework/oop/inheritance/horseAndPegas.java
@@ -1,28 +1,18 @@
 package com.test.homework.oop.inheritance;
 
-public class horseAndPegas {
-    public static void main(String[] args) {
-        class horse {
-            protected String animal = "loshadka";        // horse attribute
-
-            public void honk() {                    // horse method
-                System.out.println("igogo!");
-            }
-        }
-        class pegas extends horse {
-            private String pegasName = "Luna";    // Car attribute
-
-            public static void main(String[] args) {
-
-                // Create a myPegas object
-                pegas myPegas = new pegas();
-
-                // Call the honk() method (from the horse class) on the myPegas object
-                myPegas.honk();
-
-                // Display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class
-                System.out.println(myPegas.animal + " " + myPegas.pegasName);
-            }
-        }
+class horse {
+    protected String animal = "Horse";
+    public void honk() {
+        System.out.println("Igo-go!");
     }
 }
+
+class pegas extends horse {
+    private String horseName = "Luna";
+    public static void main(String[] args) {
+        pegas myFastCar = new pegas();
+        myFastCar.honk();
+        System.out.println(myFastCar.animal + " " + myFastCar.horseName);
+    }
+}
+
